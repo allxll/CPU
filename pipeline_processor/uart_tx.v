eca554e271697633858419bfc2f82735654a00d8
allxll <mildwindyu@hotmail.com>
2017-07-21 12:09:07 +0800

    modification pipline 100M

--- pipeline_processor/uart_tx.v
+++ pipeline_processor/uart_tx.v
@@ -1,11 +1,4 @@
-//////////////////////////////////////////////////////////////////////
-// File Downloaded from http://www.nandland.com
-//////////////////////////////////////////////////////////////////////
-// This file contains the UART Transmitter.  This transmitter is able
-// to transmit 8 bits of serial data, one start bit, one stop bit,
-// and no parity bit.  When transmit is complete o_Tx_done will be
-// driven high for one clock cycle.
-//
+
 // Set Parameter CLKS_PER_BIT as follows:
 // CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
 // Example: 10 MHz Clock, 115200 baud UART
